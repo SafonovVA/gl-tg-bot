172d8ee1a57b75810b696722991e3a6d71183e4a
vladimir.rebo <vladimir.rebo@ffins.kz>
2022-02-24 06:42:22 +0600

    removed fatal error if .env is not found

--- config/config.go
+++ config/config.go
@@ -15,7 +15,5 @@ func NewConfig() *config {
 	if err != nil {
 		log.Printf("Error loading .env file")
 	}
-	return &config{
-		TelegramBotToken: os.Getenv("TELEGRAM_BOT_TOKEN"),
-	}
+	return &config{TelegramBotToken: os.Getenv("TELEGRAM_BOT_TOKEN")}
 }
